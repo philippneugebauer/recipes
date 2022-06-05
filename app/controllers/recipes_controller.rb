e3e6e064ea3d439f7a2ddfc2a8cff82c553dda69
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-05 21:20:45 +0200

    more whitelisting for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -65,6 +65,6 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:title, :cook_time, :prep_time, :ratings, :author, :image_url)
+      params.require(:recipe).permit(:title, :cook_time, :prep_time, :ratings, :author, :image_url, :ingredients, :category_id)
     end
 end
