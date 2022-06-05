01f28a374c44d8c57e8f01d9efd20bff41b3297c
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-05 11:44:58 +0200

    add relations to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
-  has_many :recipes
+  has_many :recipes, dependent: :delete_all
+
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
 end
