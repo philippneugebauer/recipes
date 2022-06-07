9133f0ba563335e3f034df9c767cf47b9d4f3498
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-07 15:11:52 +0200

    add docs link

--- app/models/recipe_import_job.rb
+++ app/models/recipe_import_job.rb
@@ -2,6 +2,7 @@ class RecipeImportJob
   include SuckerPunch::Job
 
   # https://scoutapm.com/blog/which-ruby-background-job-framework-is-right-for-you
+  # https://edgeguides.rubyonrails.org/active_storage_overview.html
   def perform(recipe_import)
     recipe_upload_file = ActiveStorage::Blob.service.path_for(recipe_import.file.key)
     recipe_import.execute_import(recipe_upload_file)
