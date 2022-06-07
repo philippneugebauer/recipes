468d68fab4a9771c7a65062be6fcfdcd5a71e7d5
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-07 12:38:25 +0200

    delete unused recipe functionality

--- test/system/recipes_test.rb
+++ test/system/recipes_test.rb
@@ -10,42 +10,4 @@ class RecipesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Recipes"
   end
 
-  test "should create recipe" do
-    visit recipes_url
-    click_on "New recipe"
-
-    fill_in "Author", with: @recipe.author
-    fill_in "Cook time", with: @recipe.cook_time
-    fill_in "Image url", with: @recipe.image_url
-    fill_in "Prep time", with: @recipe.prep_time
-    fill_in "Ratings", with: @recipe.ratings
-    fill_in "Title", with: @recipe.title
-    click_on "Create Recipe"
-
-    assert_text "Recipe was successfully created"
-    click_on "Back"
-  end
-
-  test "should update Recipe" do
-    visit recipe_url(@recipe)
-    click_on "Edit this recipe", match: :first
-
-    fill_in "Author", with: @recipe.author
-    fill_in "Cook time", with: @recipe.cook_time
-    fill_in "Image url", with: @recipe.image_url
-    fill_in "Prep time", with: @recipe.prep_time
-    fill_in "Ratings", with: @recipe.ratings
-    fill_in "Title", with: @recipe.title
-    click_on "Update Recipe"
-
-    assert_text "Recipe was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Recipe" do
-    visit recipe_url(@recipe)
-    click_on "Destroy this recipe", match: :first
-
-    assert_text "Recipe was successfully destroyed"
-  end
 end
