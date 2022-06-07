9334c69dfd8a2115385c6e3e56817045e0e0007b
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-07 19:50:07 +0200

    fix route name since it confuses the system with parameter detection in the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :recipe_imports, only: [:new, :index, :create]
-  get '/recipes/propose', to: 'recipes#propose'
+  get '/propose_recipe', to: 'recipes#propose'
   resources :recipes, only: [:index, :show] do
   end
   root "recipes#index"
