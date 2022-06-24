9c45bbcf819cdb19f95b2d7bebf2d9c432731722
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-24 15:04:29 +0200

    improve look of recipe show

--- app/javascript/components/HeaderWithBack.tsx
+++ app/javascript/components/HeaderWithBack.tsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 const HeaderWithBack = (props) => (
   <>
-    <h1>{props.title}
-      <Link to={props.link} className="ms-2 btn btn-outline-secondary">
-        {props.linkText}
-      </Link>
-    </h1>
+    <div className="card mb-3 p-3">
+      <h1>{props.title}
+        <Link to={props.link} className="ms-2 btn btn-outline-secondary">
+          {props.linkText}
+        </Link>
+      </h1>
+    </div>
   </>
 )
 
--- app/javascript/components/RecipeShow.tsx
+++ app/javascript/components/RecipeShow.tsx
@@ -7,23 +7,25 @@ const RecipeShow = (props) => (
   <>
     <HeaderWithBack title={props.recipe.title} link={`/`} linkText={"Back to recipes"}></HeaderWithBack>
 
-    <div className="row">
-      <div className="col-6">
-        <img src={props.recipe.image_url} className="img-fluid rounded" alt={`Cooked ${props.recipe.title}`}></img>
-      </div>
-      <div className="col-6">
-        <div className="table-responsive">
-          <table className="table mt-3" aria-label="table for recipes">
-            <tbody>
-              <tr>
-                <td>Ingredients</td>
-                <td>
-                  <IngredientList ingredients={props.recipe.ingredients}></IngredientList>
-                </td>
-              </tr>
-              <RecipeDetails recipe={props.recipe}></RecipeDetails>
-            </tbody>
-          </table>
+    <div className="card">
+      <div className="row">
+        <div className="col-6">
+          <img src={props.recipe.image_url} className="img-fluid rounded" alt={`Cooked ${props.recipe.title}`}></img>
+        </div>
+        <div className="col-6">
+          <div className="table-responsive">
+            <table className="table mt-3" aria-label="table for recipes">
+              <tbody>
+                <tr>
+                  <td>Ingredients</td>
+                  <td>
+                    <IngredientList ingredients={props.recipe.ingredients}></IngredientList>
+                  </td>
+                </tr>
+                <RecipeDetails recipe={props.recipe}></RecipeDetails>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
