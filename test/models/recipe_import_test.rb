e3d1fdd8b0015bca5fa42ae08ddaf3fed94ff323
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-24 23:31:32 +0200

    write test case for the import

--- test/models/recipe_import_test.rb
+++ test/models/recipe_import_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class RecipeImportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionDispatch::TestProcess::FixtureFile
+
+  test "import" do
+    RecipeImport.new(name: "1", file: fixture_file_upload("recipes-en.json")).save!
+
+    assert RecipeImport.count == 1
+
+    RecipeImport.new().execute_import("recipes-en.json", RecipeImport.first.id)
+
+    assert RecipeImport.count == 1
+    assert RecipeImport.find(RecipeImport.first.id).status == "Successful"
+    assert Recipe.count == 10013
+    assert Category.count == 982
+  end
 end
