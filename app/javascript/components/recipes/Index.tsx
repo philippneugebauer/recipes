8fefeb238c5511efa9647812bdf0f80fb2141e80
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-25 00:36:06 +0200

    fix to className

--- app/javascript/components/recipes/Index.tsx
+++ app/javascript/components/recipes/Index.tsx
@@ -46,7 +46,7 @@ const Index = () => {
       </div>
 
       {allRecipes.length == 0 ?
-        <div class="alert alert-warning" role="alert">
+        <div className="alert alert-warning" role="alert">
           No recipes found!
         </div>
         : allRecipes}
