a3208ebe627e73a229f061f14c85ee0aa794c21d
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-06-26 10:37:07 +0200

    removed unused js

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Popper from 'popper.js'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "./app";
 
