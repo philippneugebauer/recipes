b1fbc4ea7efabcff36fd70541158332031916141
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-07-01 17:15:34 +0200

    use rails way for assets

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "./app";
 
