69d8ee57ad6eaa26f3b6646a93157f288670db0e
Philipp Neugebauer <ppn@philippneugebauer.net>
2022-07-02 10:45:30 +0200

    fallback option for not downloaded images

--- app/javascript/components/recipes/IndexItem.tsx
+++ app/javascript/components/recipes/IndexItem.tsx
@@ -3,35 +3,36 @@ import { Link } from "react-router-dom";
 import IngredientList from "./IngredientList"
 import DetailsTable from "./DetailsTable"
 
-const IndexItem = (props) => (
-  <>
-    <div key={props.recipe.id} className="card mb-3">
-      <div className="row g-0">
-        <div className="col-md-4 card-body">
-          <object data={props.recipe.image_url} type="image/png">
-            <img src={props.recipe.image_thumbnail} className="rounded float-end" alt={`Cooked ${props.recipe.title}`}></img>
-          </object>
-          <div className="card-img-overlay">
-            <h2 className="card-title h5">
-              <Link to={`/recipes/${props.recipe.id}`} aria-label="title with link to recipe details">
-                {props.recipe.title}
-              </Link>
-            </h2>
+const IndexItem = (props) => {
+  const image = props.recipe.image_thumbnail != null ? props.recipe.image_thumbnail : props.recipe.image_url
+  return (
+    <>
+      <div key={props.recipe.id} className="card mb-3">
+        <div className="row g-0">
+          <div className="col-md-4 card-body">
+            <img src={image} className="rounded float-end recipe-index__item--image" alt={`Cooked ${props.recipe.title}`}></img>
+            <div className="card-img-overlay">
+              <h2 className="card-title h5">
+                <Link to={`/recipes/${props.recipe.id}`} aria-label="title with link to recipe details">
+                  {props.recipe.title}
+                </Link>
+              </h2>
+            </div>
+          </div>
+          <div className="col-md-4 border-start my-card-border-color card-body">
+            <table className="table table-borderless" aria-label="table for recipe details">
+              <tbody>
+                <DetailsTable recipe={props.recipe}></DetailsTable>
+              </tbody>
+            </table>
+          </div>
+          <div className="col-md-4 border-start my-card-border-color card-body">
+            <IngredientList ingredients={props.recipe.ingredients}></IngredientList>
           </div>
         </div>
-        <div className="col-md-4 border-start my-card-border-color card-body">
-          <table className="table table-borderless" aria-label="table for recipe details">
-            <tbody>
-              <DetailsTable recipe={props.recipe}></DetailsTable>
-            </tbody>
-          </table>
-        </div>
-        <div className="col-md-4 border-start my-card-border-color card-body">
-          <IngredientList ingredients={props.recipe.ingredients}></IngredientList>
-        </div>
       </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default IndexItem;
